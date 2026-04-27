3a9ff6bfd036cd9e29493e77e14b0ede12776f06
Mousumi Biswas <biswasmousumi205@gmail.com>
2026-04-27 22:15:08 +0600

    Update create_tables.sql

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -1,1 +1,31 @@
-CREATE TABLE (sales, products, people, geo)
+CREATE TABLE "sales" (
+  "SPID" text,
+  "GeoID" text,
+  "PID" text,
+  "SaleDate" timestamp without time zone,
+  "Amount" integer,
+  "Customers" integer,
+  "Boxes" integer
+);
+CREATE TABLE "products" (
+  "PID" varchar(6) NOT NULL,
+  "Product" text,
+  "Category" text,
+  "Size" text,
+  "Cost_per_box" double precision,
+  PRIMARY KEY ("PID")
+);
+CREATE TABLE "people" (
+  "Salesperson" text,
+  "SPID" varchar(6) NOT NULL,
+  "Team" text,
+  "Location" text,
+  PRIMARY KEY ("SPID")
+);
+CREATE TABLE "geo" (
+  "GeoID" varchar(4) NOT NULL,
+  "Geo" text,
+  "Region" text,
+  PRIMARY KEY ("GeoID")
+);
+
